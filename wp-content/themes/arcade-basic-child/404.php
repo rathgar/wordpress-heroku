a1a8caa9da3c2b229c0195c4ad3cf12ccb5049c6
Rue Turner <rue@thinlayer.co.uk>
2014-08-02 18:11:32 +0100

    customise 404

--- wp-content/themes/arcade-basic-child/404.php
+++ wp-content/themes/arcade-basic-child/404.php
@@ -9,15 +9,16 @@ get_header(); ?>
 	<div class="container">
 		<div class="row">
 			<div id="primary" <?php bavotasan_primary_attr(); ?>>
-    			<article id="post-0" class="post error404 not-found">
-    				<i class="fa fa-frown-o"></i>
-    		    	<header>
-    		    	   	<h1 class="entry-title taggedlink"><?php _e( '404 Error', 'arcade' ); ?></h1>
-    		        </header>
-    		        <div class="entry-content description">
-    		            <p><?php _e( "Sorry. We can't seem to find the page you're looking for.", 'arcade' ); ?></p>
-    		        </div>
-    		    </article>
+				<article id="post-0" class="post error404 not-found">
+					<!---<i class="fa fa-frown-o"></i>//-->
+					<header>
+						<h1 class="entry-title taggedlink"><?php _e( '404 Error', 'arcade' ); ?></h1>
+					</header>
+					<div class="entry-content description">
+						<img src="<?php bloginfo('template_directory'); ?>-child/library/images/tube.png" alt="" width="300" height="163" />
+						<p><?php _e( "Sorry. We can't seem to find what you're looking for.", 'arcade' ); ?></p>
+					</div>
+				</article>
 			</div>
 			<?php get_sidebar(); ?>
 		</div>
