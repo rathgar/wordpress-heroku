aa3d13bdbadb12f083362744058a279a80b0d306
Rue Turner <rue@thinlayer.co.uk>
2016-10-07 10:05:02 +0100

    add favicon set

--- wp-content/themes/arcade-basic-child/functions.php
+++ wp-content/themes/arcade-basic-child/functions.php
@@ -30,4 +30,16 @@ function rue_widgets_init() {
 }
 endif; // bavotasan_widgets_init
 
+add_action('wp_head','hook_favicons');
+function hook_favicons() {
+  $output = <<<EOT
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#cc0000">
+  <meta name="theme-color" content="#cc0000">
+EOT;
+  echo $output;
+}
 ?>
