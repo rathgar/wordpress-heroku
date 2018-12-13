b146aafd74c4d6fb46d2d89bf2c5ec19674fcf51
Rue Turner <rue@thinlayer.co.uk>
2018-12-13 11:25:31 +0000

    improve sales link

--- wp-content/themes/arcade-basic-child/template-parts/content-image.php
+++ wp-content/themes/arcade-basic-child/template-parts/content-image.php
@@ -19,13 +19,17 @@
         <figcaption class="wp-caption-text artwork-caption clearfix">
           <h2><?php echo $post->post_title; ?></h2>
           <dl class="artwork-meta">
-            <dd title="Dimensions"><?php echo $post->dimensions; ?></dd>
-            <dd title="Media"><?php echo $post->media; ?></dd>
+            <?php if ($post->media != NULL) { ?>
+            <dt>Media</dt>
+            <dd><?php echo $post->media; ?></dd>
+          <?php } if ($post->dimensions != NULL) { ?>
+            <dt>Dimensions (mm)</dt>
+            <dd><?php echo $post->dimensions; ?></dd><?php } ?>
           </dl>
           <?php the_content( __( 'Read more', 'arcade-basic-child') ); ?>
 
           <?php if (has_category('for-sale')): ?>
-            <p class="for-sale">This work may be available <a href="/index.php/artwork-enquiry?artwork-id=<?php echo urlencode(get_the_ID()); ?>&artwork-title=<?php echo urlencode($post->post_title); ?>">for sale</a>.</p>
+            <p class="for-sale">"<?php echo $post->post_title; ?>" may still be available for sale - <a href="/index.php/artwork-enquiry?artwork-id=<?php echo urlencode(get_the_ID()); ?>&artwork-title=<?php echo urlencode($post->post_title); ?>">make an enquiry</a>.</p>
           <?php endif ?>
         </figcaption>
       </figure>
